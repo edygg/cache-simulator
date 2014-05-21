3ecd10c4b587be6564123be3f7472f1602bf7833
Edilson Gonzalez <edilson.gonza@unitec.edu>
2014-05-20 21:30:56 -0600

    First commit, adding main class

---
+++ src/cache/simulator/CacheSimulator.java
@@ -0,0 +1,17 @@
+
+package cache.simulator;
+
+/**
+ *
+ * @author Edilson
+ */
+public class CacheSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
