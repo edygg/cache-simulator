234f4f1852ac25f72fe2920a55a2098a42c1a0be
Edilson Gonzalez <edilson.gonza@unitec.edu>
2014-05-25 19:17:36 -0600

    Finish with test algorithm

--- src/cache/simulator/CacheSimulator.java
+++ src/cache/simulator/CacheSimulator.java
@@ -17,11 +17,16 @@ public class CacheSimulator {
     public static final Line[] cache = new Line[CACHE_LINES];
 
     public static final int BLOCK_LENGTH = 8;
-    
+    public static final int SETS = 16;
+    public static final int SET_LINES = 4;
+
     public static final byte NO_CACHE = 0;
     public static final byte DIRECT = 1;
-    public static final byte ASSOSIATIVE = 2;
-    public static final byte SET_ASSOSIATIVE = 3;
+    public static final byte ASSOCIATIVE = 2;
+    public static final byte SET_ASSOCIATIVE = 3;
+
+    public static int associativePointer = 0;
+    public static int[] setAssociativePointers;
 
     public static double time;
 
@@ -29,10 +34,16 @@ public class CacheSimulator {
         //fillRam();
         resetTime();
         test(NO_CACHE);
-        System.out.printf("%.2f\n",time);
+        System.out.printf("%.2f\n", time);
         resetTime();
         test(DIRECT);
-        System.out.printf("%.2f\n",time);
+        System.out.printf("%.2f\n", time);
+        resetTime();
+        test(ASSOCIATIVE);
+        System.out.printf("%.2f\n", time);
+        resetTime();
+        test(SET_ASSOCIATIVE);
+        System.out.printf("%.2f\n", time);
     }
 
     public static void fillRam() {
@@ -45,10 +56,13 @@ public class CacheSimulator {
             System.err.println("El archivo no se encuentra");
         }
     }
+
     public static void resetCache() {
         for (int i = 0; i < cache.length; i++) {
             cache[i] = new Line();
         }
+        associativePointer = 0;
+        setAssociativePointers = new int[16];
     }
 
     public static void resetTime() {
@@ -62,7 +76,7 @@ public class CacheSimulator {
             return ram[pos];
         } else if (type == DIRECT) {
             int line = (pos / BLOCK_LENGTH) % CACHE_LINES;
-            
+
             if (cache[line].isValid()) {
                 if (pos >= cache[line].getMemoryStart() && pos <= cache[line].getMemoryEnd()) {
                     time += 0.01;
@@ -89,12 +103,69 @@ public class CacheSimulator {
                 time += 0.66 + 0.01;
                 return ram[pos];
             }
+        } else if (type == ASSOCIATIVE) {
+            for (int i = 0; i < cache.length; i++) {
+                if (cache[i].isValid()) {
+                    if (pos >= cache[i].getMemoryStart() && pos <= cache[i].getMemoryEnd()) {
+                        time += 0.01;
+                        return ram[pos];
+                    }
+                }
+            }
+
+            if (cache[associativePointer].isValid()) {
+                if (cache[associativePointer].isModified()) {
+                    time += 0.66 + 0.66 + 0.01;
+                } else {
+                    time += 0.66 + 0.01;
+                }
+            } else {
+                time += 0.66 + 0.01;
+            }
+
+            cache[associativePointer].setMemoryStart((pos / BLOCK_LENGTH) * BLOCK_LENGTH);
+            cache[associativePointer].setMemoryEnd(cache[associativePointer].getMemoryStart() + BLOCK_LENGTH - 1);
+            cache[associativePointer].setModified(false);
+            associativePointer++;
+            if (associativePointer >= CACHE_LINES) {
+                associativePointer = 0;
+            }
+            return ram[pos];
         } else {
-            return 0;
+            int set = (pos / BLOCK_LENGTH) % SETS;
+
+            for (int i = set * SET_LINES; i < set * SET_LINES + SET_LINES; i++) {
+                if (cache[i].isValid()) {
+                    if (pos >= cache[i].getMemoryStart() && pos <= cache[i].getMemoryEnd()) {
+                        time += 0.01;
+                        return ram[pos];
+                    }
+                }
+            }
+            
+            if (cache[setAssociativePointers[set] + (set * SET_LINES)].isValid()) {
+                if (cache[setAssociativePointers[set] + (set * SET_LINES)].isModified()) {
+                    time += 0.66 + 0.66 + 0.01;
+                } else {
+                    time += 0.66 + 0.01;
+                }
+            } else {
+                time += 0.66 + 0.01;
+            }
+            
+            cache[setAssociativePointers[set] + (set * SET_LINES)].setMemoryStart((pos / BLOCK_LENGTH) * BLOCK_LENGTH);
+            cache[setAssociativePointers[set] + (set * SET_LINES)].setMemoryEnd(cache[setAssociativePointers[set] + (set * SET_LINES)].getMemoryStart() + BLOCK_LENGTH - 1);
+            cache[setAssociativePointers[set] + (set * SET_LINES)].setModified(false);
+            setAssociativePointers[set]++;
+            if (setAssociativePointers[set] >= SET_LINES) {
+                setAssociativePointers[set] = 0;
+            }
+                
+            return ram[pos];
         }
     }
 
-    public static void write(byte type, int pos,  int data) {
+    public static void write(byte type, int pos, int data) {
         if (type == NO_CACHE) {
             time += 0.1;
             ram[pos] = data;
@@ -128,6 +199,71 @@ public class CacheSimulator {
                 time += 0.66 + 0.01;
                 ram[pos] = data;
             }
+        } else if (type == ASSOCIATIVE) {
+            for (int i = 0; i < cache.length; i++) {
+                if (cache[i].isValid()) {
+                    if (pos >= cache[i].getMemoryStart() && pos <= cache[i].getMemoryEnd()) {
+                        time += 0.01;
+                        ram[pos] = data;
+                        cache[i].setModified(true);
+                        return;
+                    }
+                }
+            }
+
+            if (cache[associativePointer].isValid()) {
+                if (cache[associativePointer].isModified()) {
+                    time += 0.66 + 0.66 + 0.01;
+                } else {
+                    time += 0.66 + 0.01;
+                }
+            } else {
+                time += 0.66 + 0.01;
+            }
+
+            cache[associativePointer].setModified(true);
+            cache[associativePointer].setMemoryStart((pos / BLOCK_LENGTH) * BLOCK_LENGTH);
+            cache[associativePointer].setMemoryEnd(cache[associativePointer].getMemoryStart() + BLOCK_LENGTH - 1);
+
+            associativePointer++;
+
+            if (associativePointer >= CACHE_LINES) {
+                associativePointer = 0;
+            }
+            ram[pos] = data;
+        } else {
+            int set = (pos / BLOCK_LENGTH) % SETS;
+
+            for (int i = set * SET_LINES; i < (set * SET_LINES) + (SET_LINES - 1); i++) {
+                if (cache[i].isValid()) {
+                    if (pos >= cache[i].getMemoryStart() && pos <= cache[i].getMemoryEnd()) {
+                        time += 0.01;
+                        ram[pos] = data;
+                        cache[i].setModified(true);
+                        return;
+                    }
+                }
+            }
+            
+            if (cache[setAssociativePointers[set] + (set * SET_LINES)].isValid()) {
+                if (cache[setAssociativePointers[set] + (set * SET_LINES)].isModified()) {
+                    time += 0.66 + 0.66 + 0.01;
+                } else {
+                    time += 0.66 + 0.01;
+                }
+            } else {
+                time += 0.66 + 0.01;
+            }
+
+            cache[setAssociativePointers[set] + (set * SET_LINES)].setMemoryStart((pos / BLOCK_LENGTH) * BLOCK_LENGTH);
+            cache[setAssociativePointers[set] + (set * SET_LINES)].setMemoryEnd(cache[setAssociativePointers[set] + (set * SET_LINES)].getMemoryStart() + BLOCK_LENGTH - 1);
+            cache[setAssociativePointers[set] + (set * SET_LINES)].setModified(true);
+            setAssociativePointers[set]++;
+            if (setAssociativePointers[set] >= SET_LINES) {
+                setAssociativePointers[set] = 0;
+            }
+
+            ram[pos] = data;
         }
     }
 
@@ -155,7 +291,7 @@ public class CacheSimulator {
         int k = 0;
         for (int i = 101; i <= 115; i++) {
             k++;
-            write(type,615,  k);
+            write(type, 615, k);
             if (read(type, i) < menor) {
                 menor = read(type, i);
             }
